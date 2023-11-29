05506e85baa9b35fbd6142c29caf65ff7e6f69d7
agupeiretti <agupeiretti@gmail.com>
2023-11-29 18:34:41 -0300

    first commit

---
+++ screens/HomeScreen.js
@@ -0,0 +1,49 @@
+import { View, Text, TextInput, ScrollView } from "react-native";
+import React from "react";
+import { SafeAreaView } from "react-native-safe-area-context";
+import { StatusBar } from "expo-status-bar";
+import * as Icon from "react-native-feather";
+import { themeColor, themeColors } from "../theme";
+import Categories from "../components/categories";
+
+export default function HomeScreen() {
+  return (
+    <SafeAreaView className="bg-white">
+      <StatusBar barStyle="dark-content" />
+
+      {/* search bar */}
+
+      <View className="flex-row  items-center space-x-2 px-4 pb-2">
+        <View className="flex-row flex-1 items-center p-3 rounded-full border border-gray-300">
+          <Icon.Search height="25" width="25" stroke="gray" />
+          <TextInput placeholder="Restaurants" className="ml-2 flex-1" />
+          <View className="flex-row items-center space-x-1 border-0 border-l-2 border-gray-300">
+            <Icon.MapPin height="20" width="20" stroke="gray" />
+            <Text className="text-gray-600">Cordoba, Argentina</Text>
+          </View>
+        </View>
+        <View
+          style={{ backgroundColor: themeColors.bgColor(1) }}
+          className="p-3 bg-gray-300 rounded-full"
+        >
+          <Icon.Sliders
+            height="20"
+            width="20"
+            strokeWidth={2.5}
+            stroke="gray"
+          />
+        </View>
+      </View>
+
+      {/* main */}
+
+      <ScrollView
+        showsVerticalScrollIndicator={false}
+        contentContainerStyle={{ paddingBottom: 20 }}
+      >
+        {/* categories */}
+        <Categories />
+      </ScrollView>
+    </SafeAreaView>
+  );
+}
